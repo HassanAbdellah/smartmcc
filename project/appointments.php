aa92ba975b9fe5ff74724bdad8010e323ced668e
Hassan Abdellah <Hassan Abdellah>
2019-02-14 20:52:18 +0200

    Appointments has been added

--- project/appointments.php
+++ project/appointments.php
@@ -25,38 +25,86 @@
         max-width: 100%;
     
     }
+    table, th, td {
+        border: 3px solid black;
+        background-color: lightgrey;
+        margin: 5px;
+        padding: 5px;
+}
 
 </style>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <center>
+        <div class='userbox'>
+        <H3>Appointment</H3><br>
+            <?php
+            session_start();
 
-<div class='userbox'>
-<label>Appointment</label><br>
-    <?php 
-        session_start();
-        require 'config.php';
-        $n = mysqli_query($con, "SELECT * FROM user_reservation ORDER BY doctor_id" );
-        if(mysqli_num_rows($n)){
-            $dat_res = mysqli_fetch_assoc($n);
-            $id=$dat_res['id'];
-            $user_id = $dat_res['user_id'];
-            $doctor_id = $dat_res['doctor_id'];
-            $date=$dat_res['date'];
-            $time = $dat_res['time'];
-            $div= '<div >';
-            while($dat_res=mysqli_fetch_array($n)){
-            $div.='<div class="card" value="'.$dat_res['id'].'">'." Appointment Number : ".$dat_res['id']." User id is ".$dat_res['user_id']." and Doctor id : ".$dat_res['doctor_id']." at ".$dat_res['date']." - ".$dat_res['time'].'</div>'."<hr>";
+            if (isset($_SESSION['loginEmail'])) {
+                require 'config.php';
+                $result = mysqli_query($con, "SELECT * FROM user_reservation 
+                    WHERE user_id IN 
+                    (SELECT id FROM userinfo) ORDER BY doctor_id")
+                or die(mysqli_error());
+
+
+
+                // display data in table
+                echo "<table border='1' cellpadding='10'>";
+
+                echo "<tr> <th>ID</th> <th>USER ID</th> <th>DOCTOR ID</th> <th>DATE</th> <th>TIME</th> <th></th> <th></th></tr>";
+
+
+                // loop through results of database query, displaying them in the table
+
+                while($row = mysqli_fetch_array( $result )) {
+
+
+
+                // echo out the contents of each row into a table
+
+                echo "<tr>";
+
+                echo '<td>' . $row['id'] . '</td>';
+
+                echo '<td>' . $row['user_id'] . '</td>';
+
+                echo '<td>' . $row['doctor_id'] . '</td>';
+
+                echo '<td>' . $row['date'] . '</td>';
+
+                echo '<td>' . $row['time'] . '</td>';
+
+                echo '<td><a href="edit.php?id=' . $row['id'] . '">Edit</a></td>';
+
+                echo '<td><a href="delete.php?id=' . $row['id'] . '">Delete</a></td>';
+
+                echo "</tr>";
+
+                }
+
+                // close table>
+
+                echo "</table>";
+
+            } else {
+            header('location:error.php');
             }
-        }
-        $div.='</div>';
-        echo $div;
-    ?>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <span class="text-center">
-                <a href="welcome_Admin.php">Home</a>&nbsp;&nbsp;|&nbsp;&nbsp;
-                <a href="logout.php">Logout</a>
-            </span>
-        </div>
-    </div>
-</div>
+            ?></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <span class="text-center">
+                        <a href="welcome_admin.php">Home</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+                        <a href="logout.php">Logout</a>
+                    </span>
+                </div>
+            </div>
+        </div><center>
+            
+    </body>
+</html>
