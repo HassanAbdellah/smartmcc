c26c4543eaf54dca8d7e5a480c2f6fa703a46112
Hassan Abdellah <Hassan Abdellah>
2019-06-10 22:41:14 +0200

    ReserveD

--- project/delete_appointment.php
+++ project/delete_appointment.php
@@ -10,6 +10,12 @@ $id = $_GET['id'];
 session_start();
 if (isset($_SESSION['loginEmail'])) {
     $email = $_SESSION['loginEmail'];
+    $s = mysqli_query($con, "SELECT * FROM user_reservation WHERE id='$id'");
+    $result=mysqli_fetch_assoc($s);
+    $doctor1=$result['doctor_id'];
+    $date1=$result['date'];
+    $time1=$result['time'];
+    $i = mysqli_query($con, "INSERT  INTO available_dates (doctor_id, date, time) VALUES ('$doctor1', '$date1', '$time1')");
     $d = mysqli_query($con, "DELETE FROM user_reservation WHERE id='$id'");
     header('location:appointments_user.php');
 }
